7b7274414f23bd876861d3760f51810d56c7c040
Yakale123 <yohann@ualberta.ca>
2025-01-09 10:41:39 -0700

    Add files via upload

---
+++ requirements.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 23:23:10 2024
+
+@author: ninak
+"""
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(None) 
+random.seed()
+
+#All the instructions and parameters are included in the presentations, the outputs image we generate in the videos are also included
+#all the parameters are variable and subject to change if please
+#the markov chain probabilities can also be adjusted
+#A document with all the news used will be provided as well
+#The dictionary can also be subject to change, we left it in the code for readibility purpose after turning it into library dictionary
+#Comments were added to make the code easy to read and follow 
+#Refer to the slideshow references for any informations used and formulas
+
+#PS!!! When recording the video, i said bearish instead of bullish when explaining the markov probabilities sentiment
+#I just wanted to correct that error and clarify that i meant bullish
